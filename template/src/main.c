c32b2527299d718293a2e8551cc4023ecf05f2fd
nasso <nassomails@gmail.com>
2020-01-19 01:40:07 +0100

    feat: initial commit

---
+++ template/src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, {{YEAR}}
+** {{PROJECT_NAME}}
+** File description:
+** Program entry point
+*/
+
+#include "my/my.h"
+
+int main(void)
+{
+    my_printf("Hello, World!");
+    return (0);
+}
---
+++ template/tests/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, {{YEAR}}
+** {{PROJECT_NAME}}
+** File description:
+** Unit tests
+*/
+
+#include <criterion/criterion.h>
+
+Test({{BINARY_NAME}}, it_works)
+{
+    cr_assert_eq(2 + 2, 4);
+}
