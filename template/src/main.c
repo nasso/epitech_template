72e81ae97ae2beca1315754935977cb49a3a9de0
nasso <nassomails@gmail.com>
2020-02-05 15:04:34 +0100

    feat: add default main

---
+++ template/src/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, {{YEAR}}
+** {{PROJECT_NAME}}
+** File description:
+** Program entry point
+*/
+
+#include "my/my.h"
+#include "my/io.h"
+
+int main(void)
+{
+    my_printf("Hello World!\n");
+    return (0);
+}
